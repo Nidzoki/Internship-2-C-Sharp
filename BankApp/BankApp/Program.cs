d70ca729993e405edf040816259aef8b5b0ca0ca
Nidzoki <nikolavidovic01@gmail.com>
2024-11-11 23:23:32 +0100

    Implemented user data printout

--- BankApp/BankApp/Program.cs
+++ BankApp/BankApp/Program.cs
@@ -1,4 +1,32 @@
-﻿static void PrintMainMenu(bool error)
+﻿
+
+// test user list
+var users = new List<Tuple<int, string, string, DateTime>>()
+{
+    Tuple.Create(1, "Ante", "Antić", new DateTime(2001,09,21)),
+    Tuple.Create(2, "Pero", "Perić", new DateTime(1999,10,22)),
+    Tuple.Create(3, "Lovre", "Lovrić", new DateTime(2000,11,23)),
+    Tuple.Create(4, "Karlo", "Karlović", new DateTime(2003,12,24)),
+    Tuple.Create(5, "Karlo", "Ima vise od 30", new DateTime(1973,12,24))
+};
+
+
+//test account list
+var accounts = new List<Tuple<int, string, double>>()
+{
+    Tuple.Create(1, "žiro", 0.0),
+    Tuple.Create(1, "tekući", 2.0),
+    Tuple.Create(2, "žiro", -200.0),
+    Tuple.Create(1, "žiro", -1.0),
+    Tuple.Create(3, "žiro", 1.0),
+};
+
+
+
+
+
+// Main menu
+static void PrintMainMenu(bool error) // Prints out Main menu text
 {
     Console.Clear();
     if (error)
@@ -9,7 +37,7 @@
     
 }
 
-static void MainMenu(bool error)
+static void MainMenu(bool error, List<Tuple<int, string, string, DateTime>> users, List<Tuple<int, string, double>> accounts) // Main menu controller
 {   
     PrintMainMenu(error);
 
@@ -19,27 +47,30 @@ static void MainMenu(bool error)
     {
         case "1":
             Console.Clear();
-            UserMenu(false);
-            MainMenu(false);
+            UserMenu(false, users, accounts);
+            MainMenu(false, users, accounts);
             break;
         case "2":
             Console.Clear();
             Console.WriteLine(" Odabrali ste izbornik Računi.");
             Console.ReadKey();
             // handle option 2
-            MainMenu(false);
+            MainMenu(false, users, accounts);
             break;
         case "3":
             Console.Clear();
             Console.WriteLine(" Napustili ste aplikaciju.");
             break;
         default:
-            MainMenu(true);
+            MainMenu(true, users, accounts);
             break;
     }
 }
 
-static void PrintUserMenu(bool error)
+
+// User menu
+
+static void PrintUserMenu(bool error) // Prints out user menu
 {
     Console.Clear();
     if (error)
@@ -52,11 +83,10 @@ static void PrintUserMenu(bool error)
     Console.WriteLine(" 5 - Natrag");
 }
 
-static void UserMenu(bool error)
+static void UserMenu(bool error, List<Tuple<int, string, string, DateTime>> users, List<Tuple<int, string, double>> accounts) // User menu controller
 {
-    PrintUserMenu(false);
+    PrintUserMenu(error);
 
-    //Console.ReadKey();
     var option = Console.ReadLine();
 
     switch (option) 
@@ -83,22 +113,99 @@ static void UserMenu(bool error)
             break;
 
         case "4": // PRINT USERS
-            Console.Clear();
-            Console.WriteLine("Odabran pregled korisnika.");
-            // TODO: implement printing all users
-            Console.ReadKey();
+            UserMenuOption(false, users, accounts);
             break;
 
         case "5": // EXIT TO MAIN MENU
             break;
 
         default:
-            UserMenu(false);
+            UserMenu(true, users, accounts);
             break;
+    }
+
+}
+
+static void UserMenuOption(bool error, List<Tuple<int, string, string, DateTime>> users, List<Tuple<int, string, double>> accounts)
+{
+    Console.Clear();
+    if (error) 
+        Console.WriteLine("\n Neispravan odabir! ");
+    Console.WriteLine("\n\ta) ispis svih korisnika abecedno po prezimenu");
+    Console.WriteLine("\tb) ispis svih onih koji imaju više od 30 godina");
+    Console.WriteLine("\tc) ispis svih onih koji imaju barem jedan račun u minusu");
+    Console.WriteLine("\td) izlaz");
+
+    var option = Console.ReadLine();
+
+    switch (option) 
+    {
+        case "a":
+            Console.Clear();
+            PrintUsersBySurname(users);
+            Console.WriteLine("\n Press any key to continue...");
+            Console.ReadKey();
+            UserMenu(false, users, accounts);
+            break;
+
+        case "b": 
+            Console.Clear();
+            PrintUsersOverThirty(users);
+            Console.WriteLine("\n Press any key to continue...");
+            Console.ReadKey();
+            UserMenu(false, users, accounts);
+            break;
+        case "c":
+            Console.Clear();
+            PrintUsersInMinus(users, accounts);
+            Console.WriteLine("\n Press any key to continue...");
+            Console.ReadKey();
+            UserMenu(false, users, accounts);
+            break;
+        case "d":
+            UserMenu(false, users, accounts);
+            break;
+        default:
+            UserMenuOption(true,users, accounts);
+            break;
+    }
+}
+
+// User data printing
+
+static void PrintUsersBySurname(List<Tuple<int, string, string, DateTime>> users) // Prints out the user list alphabeticaly by surname
+ {
+    foreach (var user in users.OrderBy(x => x.Item3))
+        Console.WriteLine($" {user.Item1} - {user.Item2} - {user.Item3} - {user.Item4.ToString("dd/MM/yyyy")}");
+}
+
+static void PrintUsersOverThirty(List<Tuple<int, string, string, DateTime>> users) // Prints out users in the user list that are 30+ years old
+{
+    foreach (var user in users.Where(x => (DateTime.Today - x.Item4) >= new TimeSpan(30*365,0,0,0)))
+        Console.WriteLine($" {user.Item1} - {user.Item2} - {user.Item3} - {user.Item4.ToString("dd/MM/yyyy")}");
+}
+
+static void PrintUsersInMinus(List<Tuple<int, string, string, DateTime>> users, List<Tuple<int, string, double>> accounts) // Prints out users that own accounts which are in minus
+{
+    var visited = new List<int>();
+
+    foreach(var account in accounts.Where(x => x.Item3 < 0))
+    {
+        if(visited.Contains(account.Item1))
+            continue;
+        visited.Add(account.Item1);
+
+        var user = users.FirstOrDefault(x => x.Item1 == account.Item1, null);
+
+        if (user != null)
+            Console.WriteLine($" {user.Item1} - {user.Item2} - {user.Item3} - {user.Item4.ToString("dd/MM/yyyy")}");
+
     }
 
 }
 
 
 
-MainMenu(false);
+// Start of program
+
+MainMenu(false, users, accounts);
